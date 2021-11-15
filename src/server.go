7513e09ebdb6a30d31ff11f69e9df5cf2aa3cf7c
Luis Quinones <lpaandres2020@gmail.com>
2021-11-15 17:34:04 -0500

    Add: DELETE one METHOD. Mod: GET one METHOD(the alternative return).

--- src/server.go
+++ src/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"strconv"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -41,7 +42,17 @@ func main() {
 				return c.JSON(http.StatusOK, courseitem)
 			}
 		}
-		return c.JSON(http.StatusOK, courses)
+		return c.String(http.StatusOK, "Bad data.")
+	})
+
+	e.DELETE("/courses/:id", func(c echo.Context) error {
+		for i, courseitem := range courses {
+			if strconv.Itoa(courseitem.Id) == c.Param("id") {
+				courses = append(courses[:i], courses[i+1])
+				return c.JSON(http.StatusOK, courses)
+			}
+		}
+		return c.String(http.StatusOK, "The indicated course doesn't exist.")
 	})
 
 	e.Start(":2000")
